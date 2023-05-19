894863d90d5dd8d3de267e42ce19dd6baa84d0c0
Kassandra Haftner <Kassandra.Haftner@outlook.com>
2023-05-18 19:48:45 -0700

    Individual Movie page changes.

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,29 +1,62 @@
 import { BASE_URL_INDIVIDUAL_SIZE } from "../globals/globalVariables";
+import { BASE_URL_ORIGINAL_SIZE } from "../globals/globalVariables";
 import { appTitle } from "../globals/globalVariables";
+import "../styles/SingleMovie.css";
+import { Link } from 'react-router-dom';
+import FavButton from "./FavButton";
 
 const MovieInfo = (props) => {
   const { movie } = props;
 
+  // for each item (object) in array genreArray do object stuff
+	const genreArray = movie.genres;
+	// console.dir(movie);
+	let movieGenres = [];
+  let thisMovieGenres = [];
+	for (let index = 0; index < genreArray.length; index++) {
+		const movieGenre = genreArray[index];
+		// console.log(movieGenre.name);
+		movieGenres.push(movieGenre.name);
+		thisMovieGenres = movieGenres.map((oneGenre) => (
+			<li>{oneGenre}</li>
+		));
+	}
+	// console.log(movieGenres);
+
+  const heroBackgroundStyle = {
+    backgroundImage: `url(${BASE_URL_ORIGINAL_SIZE}${movie.poster_path})`,
+  };
+
+  document.title = `${appTitle} - ${movie.title}`; 
+
   return (
     <>
-      <section className="movie-info">
-        <div className="single-movie-container" key={movie.id}>
-          <div className="medium-movie-poster">
-            <img src={`${BASE_URL_INDIVIDUAL_SIZE}${movie.poster_path}`} alt="movie" />
-          </div>
+      <section className="movie-info" key={movie.id}>
+        <div className="single-movie-container">
+          <div className="hero-background" style={heroBackgroundStyle}></div>
+          <div className="movie-flex-container">
+            <Link to={`${BASE_URL_ORIGINAL_SIZE}${movie.poster_path}`} target="_blank"><img className="poster-image" src={`${BASE_URL_INDIVIDUAL_SIZE}${movie.poster_path}`} alt="movie" /></Link>
 
-          <div className="movie-info">
-            <div className="movie-title-box">
-              <p><span><strong>{movie.title}</strong></span></p>
-              <p>Status: {movie.status}</p>
+            <div className="movie-details">
+              <div className="single-movie-title-box">
+                <p><span><strong>{movie.title}</strong></span></p>
+              </div> 
+              <div className="movie-details-container">
+                <p><strong>Status</strong>: {movie.status}</p>
+                <p><strong>Release Date</strong>:<br /><em>{movie.release_date}</em></p>
+                <p>
+                <strong>Average Rating</strong>:{" "}
+                  {movie.vote_average}
+						    </p>
+                <p><strong>Overview</strong>: {movie.overview}</p>
+                <p><strong>Budget</strong>: ${movie.budget}</p>
+                <p><strong>Revenue</strong>: ${movie.revenue}</p>
+                <p>
+							    <strong>Genres</strong>: <ul>{thisMovieGenres}</ul>
+						    </p>
+                <p><strong>Runtime</strong>: {movie.runtime} minutes</p>
+              </div>
             </div>
-            <p>Release Date:<br /><em>{movie.release_date}</em></p>
-            <p>Average Rating: <strong>{movie.vote_average}</strong></p>
-            <p>Overview: {movie.overview}</p>
-            <p>Budget: ${movie.budget}</p>
-            <p>Revenue: ${movie.revenue}</p>
-            {/* <p>Genres: {movie.genres (array with genre id and genre name )}</p> */}
-            <p>Runtime: {movie.runtime} minutes</p>
           </div>
         </div>
       </section>
