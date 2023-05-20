eb0e66285f58624c65d6534d1a6a84c6131d1286
Kassandra Haftner <Kassandra.Haftner@outlook.com>
2023-05-19 23:13:20 -0700

    Added mobile menu!

--- src/components/NavMain.js
+++ src/components/NavMain.js
@@ -1,12 +1,10 @@
 import "../styles/NavMain.css";
-import MenuButton from "../components/MenuButton";
 import { NavLink } from "react-router-dom";
 
 function NavMain() {
 	return (
 		<div>
-			<MenuButton />
-
+ 
 			<nav className="site-navigation" id="site-navigation">
 				<ul className="nav-list" id="nav-list">
 					<li>
@@ -21,14 +19,6 @@ function NavMain() {
 				</ul>
 			</nav>
 
-			{/* Not finished yet
-      <div className="search-container">
-        <form action="#">
-          <input type="text" placeholder="Search.." name="search" />
-          <button type="submit" input="Search"></button>
-        </form>
-      </div>
-  */}
 		</div>
 	);
 }
