a75a4b5ad82edad90fcf96147539e0dddba16600
amyturton <amy.grace.elizabeth@gmail.com>
2023-05-19 21:35:48 -0700

    woohoo got build working

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { APP_FOLDER_NAME } from "./globals/globalVariables";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename={`/${APP_FOLDER_NAME}`}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
